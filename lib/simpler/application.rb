573c895d01c7a808a7c95d168129788af533a2d6
NazarovSV <badger.lock@yandex.ru>
2021-02-25 01:39:59 +0300

    логирование

--- lib/simpler/application.rb
+++ lib/simpler/application.rb
@@ -28,10 +28,10 @@ module Simpler
     def call(env)
       route = @router.route_for(env)
       return default if route.nil?
-
+      
       controller = route.controller.new(env)
       action = route.action
-
+      
       make_response(controller, action)
     end
 
